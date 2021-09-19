a1312bd397a38c83bc5ac97b954c87777ee1f63d
April Hughes <ahughes0389@gmail.com>
2021-09-19 12:59:04 -0700

    adjusted img size in index.css

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -2,7 +2,7 @@ const products = [
   {
     _id: '1',
     name: 'Lavender Flowers',
-    image: '/images/lavender.jpg',
+    image: '/images/lavender2.jpg',
     description:
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     brand: 'Apple',
@@ -28,7 +28,7 @@ const products = [
   {
     _id: '3',
     name: 'Sage',
-    image: '/images/sage.jpg',
+    image: '/images/sage2.jpg',
     description:
       'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
     brand: 'Cannon',
@@ -54,7 +54,7 @@ const products = [
   {
     _id: '5',
     name: 'Elder Flower',
-    image: '/images/elderFlower.jpg',
+    image: '/images/elderFlower2.jpg',
     description:
       'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
     brand: 'Logitech',
