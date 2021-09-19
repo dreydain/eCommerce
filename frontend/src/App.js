f43ce661c6d5d074f5265c67f982a98e37e04f56
April Hughes <ahughes0389@gmail.com>
2021-09-19 11:12:25 -0700

    created header footer, adding navebar, imported bootstrap and font awesome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import {Container} from 'react-bootstrap'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Wilderness Apothecary</h1>
+    <Header />
+    <main className='py-3'>
+      <Container>
+        <h1>Welcome to Wilderness Apothecary</h1>
+      </Container>
+    </main>
+    <Footer />
+      
     </>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
