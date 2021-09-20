6b3523ca6b5da0df0b3697c02a0084d6cd6d9efe
April Hughes <ahughes0389@gmail.com>
2021-09-19 17:34:53 -0700

    added Link and LinkContainer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import {Nav, Navbar, Container} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 const Header = () => {
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' CollapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">Wilderness Apothecary</Navbar.Brand>                    
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>Wilderness Apothecary</Navbar.Brand>
+                    </LinkContainer>                    
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
                     <Navbar.Collapse id='basic-navbar-nav'>
                         <Nav className='m-auto'>
-                            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-                            <Nav.Link href="/login"><i className='fas fa-user'></i>Sign In</Nav.Link>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
