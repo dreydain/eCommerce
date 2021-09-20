5d1fb94f25417eed5a07c95b180a988de9e3333e
April Hughes <ahughes0389@gmail.com>
2021-09-19 18:47:14 -0700

    added proxy server to get around cors policy, and created useEffect axios get request functions for homescreen and product screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import React from 'react'
-import products from '../products'
-import {Row, Col} from 'react-bootstrap'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import Product from '../components/Product'
+import {Row, Col} from 'react-bootstrap'
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const {data} = await axios.get('/api/products')
+
+            setProducts(data)
+        }
+
+        fetchProducts()
+    }, [])
+
     return (
         <>
             <h1>Latest Products</h1>
