2d3522bec36e826d84e0f9cd3a281a8ba2c7176d
April Hughes <ahughes0389@gmail.com>
2021-09-19 18:06:45 -0700

    added ProductScreen, used tiernary operators to toggle btn disable based off of stock numbers

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -11,7 +11,7 @@ const HomeScreen = () => {
                 {products.map((product => 
                     <Col key={product._id} sm={12} md={6} lg={4}
                     xl={3}>
-                        <Product product={product}/>
+                        <Product product={product} fluid/>
                     </Col>
                 ))}
             </Row>
