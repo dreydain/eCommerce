66ffb12edbbd9d26e4941661e959977b1e2fa0e1
April Hughes <ahughes0389@gmail.com>
2021-09-20 13:38:46 -0700

    created seeder to import and destroy data through the terminal

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,18 +4,18 @@ const users = [
     {
         name: 'Admin User',
         email: 'admin@example.com',
-        password: bycrypt.hashSync('123456', 10),
+        password: bcrypt.hashSync('123456', 10),
         isAdmin: true
     },
     {
         name: 'John Doe',
         email: 'john@example.com',
-        password: bycrypt.hashSync('123456', 10),
+        password: bcrypt.hashSync('123456', 10),
     },
     {
         name: 'Jane Doe',
         email: 'jane@example.com',
-        password: bycrypt.hashSync('123456', 10),
+        password: bcrypt.hashSync('123456', 10),
     },
 ]
 
