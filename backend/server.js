ffec48ddbe26436ebacd93911457a6258d0b268c
April Hughes <ahughes0389@gmail.com>
2021-09-21 16:13:35 -0700

    added custom error handling

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -14,13 +15,15 @@ app.get('/', (req, res) => {
     res.send('api is running')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
+app.use((err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack, 
+    })
 })
 
 const PORT = process.env.PORT || 5000
