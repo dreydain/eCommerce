31c691d014ee934e5e528339896ab10b6702aaa0
April Hughes <ahughes0389@gmail.com>
2021-09-24 12:51:13 -0700

    corrected typo

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,6 @@
 import { CART_ADD_ITEM } from "../constants/cartConstants"
 
-export const cartReducer = (state = {cartItems: [], action}) => {
+export const cartReducer = (state = {cartItems: []}, action) => {
     switch(action.type) {
         case CART_ADD_ITEM:
             const item = action.payload
