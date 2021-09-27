c6c12503aedbc43ff860e1b56d1015b1c646f122
April Hughes <ahughes0389@gmail.com>
2021-09-27 10:57:49 -0700

    corrected qty typo in product screen and imported files needed for cartScreen and added functionality.

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -9,7 +9,7 @@ import { listProductDetails } from '../actions/productActions'
 
 
 const ProductScreen = ({match, history}) => {
-    const [qty, setQty] = useState(0)
+    const [qty, setQty] = useState(1)
     const dispatch = useDispatch()
 
     const productDetails = useSelector(state => state.productDetails)
