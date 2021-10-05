3b30bbbc2bd662170974e1bac6c41762c932d684
April Hughes <ahughes0389@gmail.com>
2021-10-04 20:37:28 -0700

    created checkoutSteps component and imported into shipping screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -4,7 +4,7 @@ import {Form, Button} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 import FormContainer from '../components/FormContainer'
 import {saveShippingAddress} from '../actions/cartActions'
-
+import CheckoutSteps from '../components/CheckoutSteps'
 
 const ShippingScreen = ({history}) => {
     const cart = useSelector(state => state.cart)
@@ -25,6 +25,7 @@ const ShippingScreen = ({history}) => {
 
     return (
         <FormContainer>
+            <CheckoutSteps step1 step2 />
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
             <Form.Group controlId='address'>
