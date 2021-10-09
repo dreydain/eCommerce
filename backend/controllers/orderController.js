dd96e5548ce8829d77d53bbc5b2ef5b12e62d62f
April Hughes <ahughes0389@gmail.com>
2021-10-09 16:53:39 -0700

    created the updateOrderToPaid Controller and added it to orderRoutes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import { set } from 'mongoose'
 import Order from '../models/orderModel.js'
 
 //@desc Fetch Create new order
@@ -54,7 +55,33 @@ const getOrderById = asyncHandler(async (req, res) => {
 
 })
 
+//@desc Update order to paid
+//@route GET /api/orders/:id/pay
+//@access Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if(order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        }
+
+        const updatedOrder = await order.save()
+        res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+
+})
+
 export {
     addOrderItems,
     getOrderById,
+    updateOrderToPaid,
 }
