66aa05c9581ed22a2698ad07f009e18d14af2fe0
April Hughes <ahughes0389@gmail.com>
2021-10-10 09:44:25 -0700

    created a get all users controller and route, along with admin middleware. functions properly in postman

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -105,9 +105,19 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
+//@desc Get all users
+//@route  GET /api/users
+//@access Private/admin
+const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({})
+    res.json(users)
+
+})
+
 export {
     authUser,
     getUserProfile,
     registerUser,
     updateUserProfile,
+    getUsers,
 }
