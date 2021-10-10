ab363ece12d59c75784ecbdde99ea79558fbaa0d
April Hughes <ahughes0389@gmail.com>
2021-10-10 13:25:22 -0700

    deleteUser controller and route created, functioning in postman

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -114,10 +114,27 @@ const getUsers = asyncHandler(async (req, res) => {
 
 })
 
+//@desc Delete User
+//@route  Delete /api/users/:id
+//@access Private/admin
+const deleteUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id)
+    
+    if(user) {
+        await user.remove()
+        res.json({message:'User removed' })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+
+})
+
 export {
     authUser,
     getUserProfile,
     registerUser,
     updateUserProfile,
     getUsers,
+    deleteUser,
 }
