66b92424a84a4cd3ceaabd383e22812482ddfda8
April Hughes <ahughes0389@gmail.com>
2021-10-11 13:04:28 -0700

    created updateUser and getUserById controller and routes. Both are functioning in postman

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -130,6 +130,48 @@ const deleteUser = asyncHandler(async (req, res) => {
 
 })
 
+//@desc Get user by id
+//@route Get /api/users/:id
+//@access Private/admin
+const getUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id).select('-password')
+    
+    if(user) {
+        res.json(user)
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+
+})
+
+//@desc Update user
+//@route  PUT /api/users/:id
+//@access Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id)
+
+    if(user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        // user.isAdmin = req.body.isAdmin
+        //said isAdmin was required when trying to update in postman.
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        })
+
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 export {
     authUser,
     getUserProfile,
@@ -137,4 +179,6 @@ export {
     updateUserProfile,
     getUsers,
     deleteUser,
+    getUserById,
+    updateUser,
 }
