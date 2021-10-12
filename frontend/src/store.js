f7457b7a8eb903662076d75e2645dc9821ee2b74
April Hughes <ahughes0389@gmail.com>
2021-10-12 14:01:10 -0700

    created updateProduct constants, reducers, actions and added to store and ProductEditScreen, update is functioning properly in front end

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,6 +6,7 @@ import {
     productDetailsReducer,
     productDeleteReducer,
     productCreateReducer,
+    productUpdateReducer,
 } from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
 import { 
@@ -31,6 +32,7 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     productDelete: productDeleteReducer,
     productCreate: productCreateReducer,
+    productUpdate: productUpdateReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
