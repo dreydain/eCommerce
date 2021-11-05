84bb180f0b69e96cae606c49f7994343b74a564a
April Hughes <ahughes0389@gmail.com>
2021-11-05 12:20:42 -0700

    Database set up for local host, the dojo way

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect("mongodb://localhost/" + process.env.DB_NAME, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
         })
