64f3581ac45699b5f56c980ed9cc3cff23f5d1a1
April Hughes <ahughes0389@gmail.com>
2021-11-05 13:47:15 -0700

    connecting to mongo_uri again to access db for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose'
-
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb://localhost/" + process.env.DB_NAME, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
         })
